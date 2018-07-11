c9ae923bca9d64d561d9349531a92c040f1276e0
Masato Takahashi <masato.takaha@gmail.com>
2018-07-11 16:38:37 -0700

    refactor using functions and using arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,33 @@
 #!/user/bin/env python
 
 import subprocess
+import optparse
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+
+def get_arguments():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+
+    if not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more info.")
+
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for {0} to {1}".format(interface, new_mac))
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
