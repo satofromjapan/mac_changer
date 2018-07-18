8554f88c046af41240388d66790628a6ea3edd67
Masato Takahashi <masato.takaha@gmail.com>
2018-07-17 21:18:39 -0700

    added the ability to choose a random mac address

--- mac_changer.py
+++ mac_changer.py
@@ -3,13 +3,14 @@
 import subprocess
 import optparse
 from re import search
+from random import randint
 
 
 def get_arguments():
     parser = optparse.OptionParser()
 
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address, you can also use 'random' to generate a random MAC Address")
 
     (options, arguments) = parser.parse_args()
 
@@ -38,17 +39,25 @@ def get_current_mac(interface):
     else:
         print("[-] Could not read MAC address")
 
+def randomMAC():
+    generated_mac = [0x00, 0x16, 0x3e, randint(0x00, 0x7f), randint(0x00, 0xff), randint(0x00, 0xff)]
+    return ':'.join(map(lambda x: "%02x" % x, generated_mac))
 
 options = get_arguments()
 
 current_mac = get_current_mac(options.interface)
 print("Current MAC = {0}".format(current_mac))
 
-change_mac(options.interface, options.new_mac)
+if options.new_mac == 'random':
+    altered_mac = randomMAC()
+    change_mac(options.interface, altered_mac)
+else:
+    altered_mac = options.new_mac
+    change_mac(options.interface, altered_mac)
 
 current_mac = get_current_mac(options.interface)
 
-if current_mac == options.new_mac:
+if current_mac == altered_mac:
     print("[+] MAC address was successfully changed to {0}".format(current_mac))
 else:
     print("[-] MAC address did not get changed.")
